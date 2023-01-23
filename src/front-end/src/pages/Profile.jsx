c0abeb64c23d56b7297f995f4d6b5b7658d0470c
spencer-hurd <schurd02@gmail.com>
2023-01-23 14:27:47 -0600

    adding elements to routes

--- src/front-end/src/pages/Profile.jsx
+++ src/front-end/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-const Products = () => {
+const Profile = () => {
     return (
         <div>
 
@@ -6,4 +6,4 @@ const Products = () => {
     )
 }
 
-export default Products
+export default Profile
