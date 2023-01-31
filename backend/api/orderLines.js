e76eb67a91312b5ee40a90b28e5caf0ac6ff4b37
Delores Curtis <dcurtis1920@gmail.com>
2023-01-31 11:49:02 -0500

    api routes done

---
+++ backend/api/orderLines.js
@@ -0,0 +1,38 @@
+const express = require ("express");
+const orderLinesRouter = express.Router();
+const { requireUser } = require('./helpers');
+const {createOrderLine, updateOrderLineQuantity, getOrderLinesByOrderId} = ('../db/orderItems')
+
+orderLinesRouter.use((req, res, next) => {
+  console.log("A request is being made to /orderLines.");
+next();
+});
+
+// POST /api/cartItems
+orderLinesRouter.post('/', requireUser, async (req, res, next) => {
+  const requestBody = req.body;
+  
+  try {
+      const newOrderLine = await createOrderLine(requestBody);
+      console.log(newOrderLine);
+      res.send(newOrderLine);
+  } catch ({name, message}) {
+      next({name, message});
+  }
+})
+
+
+// GET /api/cartItems/:sessionId
+orderLinesRouter.get('/:orderId', requireUser, async (req, res, next) => {
+  const orderId = req.params.orderId;
+  console.log(orderId, "orderId Here")
+
+  try {
+      const orderLines = await getOrderLinesByOrderId(orderId);
+      res.send(orderLines);
+  } catch ({name, message}) {
+      next({name, message});
+  }
+})
+
+module.exports = orderLinesRouter
--- backend/db/orderItems.js
+++ backend/db/orderItems.js
@@ -16,6 +16,21 @@ async function createOrderLine({orderId, productId, quantity}) {
     }
 }
 
+async function getOrderLinesByOrderId(orderId){
+  try {
+    const {rows: orderLines} = await client.query(`
+      SELECT order_lines.*, products.id as "productId", products.name, products.price, products.sku, products."photoURL"
+      FROM order_lines
+      WHERE "orderId" = $1
+      JOIN products on products.id = order_lines."productId";`, [orderId])
+    return orderLines;
+  } catch (error) {
+    console.error('There was an error in getOrderLinesByOrderId', error);
+    throw error;
+  }
+}
+
 module.exports = {
     createOrderLine,
+    getOrderLinesByOrderId,
 };
