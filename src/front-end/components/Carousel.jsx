140fd22c31ec3b60da40a4466db57ca44097cb4b
Delores Curtis <dcurtis1920@gmail.com>
2023-02-05 21:21:14 -0500

    css

---
+++ src/front-end/components/Carousel.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './styles/Carousel.css'
+
+
+const Carousel = () => {
+  return (
+    <div className='wrapper'>
+      <div className="carousel" >
+        <div className='panel'>3D -</div>
+        <div className='panel'>GLASSES</div>
+        <div className='panel'></div>
+        <div className='panel'>3D -</div>
+        <div className='panel'>GLASSES</div>
+        <div className='panel'></div>
+        <div className='panel'>3D -</div>
+        <div className='panel'>GLASSES</div>
+        <div className='panel'></div>
+    
+      </div> 
+      
+       </div>
+  )
+}
+
+
+export default Carousel
